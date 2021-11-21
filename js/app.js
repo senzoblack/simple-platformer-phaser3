eec7394af03f514d0192444a6545181a2bd9c71b
Senzo <senzo.mthembu@playsafesa.com>
2021-11-21 16:09:22 +0200

    Latest changes

--- js/app.js
+++ js/app.js
@@ -7,7 +7,6 @@ var config = {
         create: create,
         update: update
     }
-
 };
 var game = new Phaser.Game(config);
 function preload() {
@@ -19,5 +18,68 @@ function preload() {
         frameWidth:32,frameHeight:48
     });
 }
-function create(){}
-function update(){}
+function create(){
+    this.add.image(400,300,'sky');
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400,568,'ground').setScale(2).refreshBody();
+
+    platforms.create(600,400,'ground');
+    platforms.create(50,250,'ground');
+
+    player =  this.physics.add.sprite(100,450,'dude');
+
+    player.setBounce(0.2);
+    player.setColliderWorldBounds(true);
+
+    this.anims.create({
+        key:'left',
+        frames: this.anims.generateFrameNumbers('dude',{start:0,end:3}),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'turn',
+        frames:[{key:'dude',frame:4}],
+        framerate:20
+    });
+    this.anims.create({
+        key:'right',
+        frames:10,
+        repeat: -1
+    });
+    cursors = this.input.keyboard.createCursorKeys();
+    stars = this.physics.add.group({
+        key:'star',
+        repeat:11,
+        setXY:{x:12, y:0, stepX:70}
+    });
+    stars.children.iterate(function(){
+        child.setBounceY(Phaser.MathFloatBetween(0.4,0.8));
+    });
+    scoreText = this.add.text(16,16,'score:0',{fontSize:'32px',fill:'#000'});
+
+    this.physics.add.collider(player,platforms);
+    this.physics.add.collider(stars,platforms);
+
+    this.physics.add.overlap(player,stars,collectStar,null,this);
+}
+function update(){
+    if(cursors.left.isDown){
+        player.setVelocityX(-160);
+        player.anims.play('left',true);
+    }else if(cursors.right.isDown){
+        player.setVelocityX(160);
+        player.anims.play('right',true);
+    }else{
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }if(cursors.up.isDown && player.body.touching.down){
+        player.setVelocityY(-330);
+    }
+    function collectStar(player, star){
+        star.disableBody(true,true);
+        score += 10;
+        scoreText.setText('Score:' + score);
+    }
+}
